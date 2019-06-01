eada77e7a51dd51789681541d471c533edfccb9f
Edward Perez <edward.121489@gmail.com>
2019-06-01 17:22:11 +0200

    added public as static files and connect routes folder

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes/index.js');
+const port = 3000;
+
+// Template engine set to Pug
+app.set('view engine', 'pug');
+// Static files set to public folder
+app.use('/static', express.static('public'));
+// Routes
+app.use(routes);
+
+
+// App listening on port variable
+app.listen(port, console.log(`Express app is listening on ${port}`));
