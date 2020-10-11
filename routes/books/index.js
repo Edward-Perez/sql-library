ee88140c56154e62f4467c4af88cdffc39707125
develop <dev@dev.local>
2020-10-11 15:19:08 +0200

    Rearrange middleware on new line for readability

--- routes/books/index.js
+++ routes/books/index.js
@@ -1,25 +1,31 @@
 const router = require('express').Router({ strict: true });
 const book = require('../../controllers/book/book')
 
-router.get('/search', book.search, book.showSearchResults);
+router.get('/search', 
+    book.search, 
+    book.showSearchResults);
 
-router.get('/page/:pageIndex', book.getList, book.showList);
+router.get('/page/:pageIndex', 
+    book.getList, 
+    book.showList)
 
 router.route('/add')
     .get(book.showAddForm)
-    .post(book.add);
+    .post(book.add)
 
 router.route('/:bookId/edit') 
     .all(book.getBookById)
     .get(book.showEditForm)
-    .post(book.edit);
+    .post(book.edit)
 
-router.post('/:bookId/delete', book.getBookById, book.delete);
+router.post('/:bookId/delete', 
+    book.getBookById, 
+    book.delete)
 
 router.use((err, req, res, next) => {
     if(err.name === "SequelizeValidationError") {
         req.userErrors = err.errors.map(arg => arg.message);
-        
+
         if (req.path === "/add") {
             book.showAddForm(req, res)
         } else if (req.path === "/edit") {
