cb2529b3f3f206e94eebb3340fe135feed4dccaf
develop <dev@dev.local>
2020-10-11 14:42:05 +0200

    Rename / delete

--- routes/books/searchBook.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../../controllers');
-
-// Search Book Get Request
-router.get('/', controller.searchBooks);
-
-module.exports = router;
