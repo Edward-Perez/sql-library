76c0ba803f0d4e77b64058f4af31fbc86bd8fb35
edward-perez <edward.p.dev@gmail.com>
2020-10-12 11:41:18 +0200

    created pugBookObject  to handle pug render objs

--- helperFunctions/index.js
+++ helperFunctions/index.js
@@ -3,5 +3,14 @@ module.exports = {
         const err = new Error(message);
         err.status = status;
         return err;
-    }
+    },
+
+    pugBookObject: (req = {}) => 
+        Object.create({
+            book: req.book, 
+            pagination: req.pagination,
+            input: req.body, 
+            searchResults:  req.searchResults,
+            errorMsg:  req.userErrors
+    })
 }
